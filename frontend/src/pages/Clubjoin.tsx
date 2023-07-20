8dd1fd18784f63624fff25507ee796f2dee44bc7
whtnqls124578 <whtnqls124578@naver.com>
2023-07-20 23:25:09 +0900

    페이지 라우터 설정 및 페이지추가

---
+++ frontend/src/pages/Clubjoin.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Clubjoin = () => {
+  return (
+    <div className='clubjoin'>
+      여기는 신청
+    </div>
+  )
+}
+
+export default Clubjoin;
---
+++ frontend/src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div className='main'>
+      여기는 메인
+    </div>
+  )
+}
+
+export default Main;
