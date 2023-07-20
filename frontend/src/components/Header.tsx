21ba78a7702b73ad00082d4e0bbc65a2a0f38243
whtnqls124578 <whtnqls124578@naver.com>
2023-07-21 01:54:12 +0900

    header Ui 작업 및 하위컴포넌트 생성

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,21 @@
+import '../css/header.css';
+import HeaderBtn from './HeaderBtn';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className='header_container'>
+                <div className='logo'>
+                    로고
+                </div>
+                <div className='header_menu'>
+                    <HeaderBtn address={'/clubjoin'} text={'동아리 신청'}/>
+                    <HeaderBtn address={'/detail'} text={'동아리'}/>
+                    <HeaderBtn address={'/mypage'} text={'MY'}/>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
