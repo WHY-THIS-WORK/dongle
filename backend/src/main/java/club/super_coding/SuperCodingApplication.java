5f8d09e23efcb6c5145cd5e950293da7f76adf25
김주환 <kjh71047@naver.com>
2023-07-22 03:23:43 +0900

    slf4j 추가
    slf4j 추가하여 개발 도중 데이터를 읽어볼 수 있습니다.

--- backend/src/main/java/club/super_coding/SuperCodingApplication.java
+++ backend/src/main/java/club/super_coding/SuperCodingApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 @EntityScan(basePackages = {"club.super_coding.entity"})
 
 public class SuperCodingApplication {
-
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(SuperCodingApplication.class);
         SpringApplication.run(SuperCodingApplication.class, args);
     }
 
