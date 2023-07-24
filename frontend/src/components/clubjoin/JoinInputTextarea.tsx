f3c82947efe99aadfc23018a5e6fb818a3734ff9
whtnqls124578 <whtnqls124578@naver.com>
2023-07-25 03:27:25 +0900

    clubjoin 컴포넌트 분리 완료 / 컴포넌트 폴더생성 후 이동

---
+++ frontend/src/components/clubjoin/JoinInputTextarea.tsx
@@ -0,0 +1,11 @@
+import '../../css/JoinInputTextarea.css';
+
+const JoinInputTextarea = () => {
+    return (
+        <div className="clubjoin_input-value">
+            <textarea placeholder='동아리를 통해 하고 싶은것들을 적어주세요.' />
+        </div>
+    )
+}
+
+export default JoinInputTextarea;
