3a7881a2f3d9b2ad55dccd1003c3b9a5be6008c5
kimdaehane <smry2012@naver.com>
2023-07-26 00:55:48 +0900

    로그인 및 토큰 조회 구현
    로그인과 토큰 조회 구현을 하였습니다

---
+++ backend/src/main/java/club/super_coding/dto/ResponseDto.java
@@ -0,0 +1,24 @@
+package club.super_coding.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+
+@AllArgsConstructor(staticName ="set") //이름 고정
+@Data
+public class ResponseDto<D> {
+    private boolean result;
+    private String message;
+    private D data;
+
+    //성공 인스턴스
+    public static <D> ResponseDto<D> setSuccess(String message, D data){
+        return ResponseDto.set(true ,message,data);
+    }
+    //실패 인스턴스
+    public static <D> ResponseDto<D> setFailed(String message){
+        return ResponseDto.set(false,message,null);
+    }
+}
