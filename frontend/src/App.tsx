55330fbefd4428605e5d07fbfa3f6fb77cf61f0e
seod4423 <seod4423@gmail.com>
2023-07-26 03:22:20 +0900

    긴급: backend dependency 추가, main.java 삭제, conflict 해결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
-import Main from './pages/Main';
-import Login from './pages/Login';
-import Mypage from './pages/Mypage';
-import Signup from './pages/Signup';
-import Detail from './pages/Detail';
-import Clubjoin from './pages/Clubjoin';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import Mypage from "./pages/Mypage";
+import Signup from "./pages/Signup";
+import Detail from "./pages/Detail";
+import Clubjoin from "./pages/Clubjoin";
 
 function App() {
+  // TODO: 동아리 detail(소개, 동아리 가입신청페이지) path variable 연결
+  // TODO: 동아리페이지(동아리 main) path variable 연결
+  // TODO: 동아리페이지 템플릿(사이드 바, 게시판)
+  // TODO: 동아리게시판 작업
 
   return (
     <div className='App'>
@@ -23,7 +27,7 @@ function App() {
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
