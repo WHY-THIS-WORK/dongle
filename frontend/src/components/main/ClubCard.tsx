2f284ad521bd4b6948f393927fc7a2703ecca155
whtnqls124578 <whtnqls124578@naver.com>
2023-07-27 15:55:48 +0900

    cardList 임시데이터 채우기 완료

--- frontend/src/components/main/ClubCard.tsx
+++ frontend/src/components/main/ClubCard.tsx
@@ -1,28 +1,34 @@
 import React from 'react'
 import '../../css/clubCard.css';
 
-const ClubCard = () => {
+type cardList = {
+    title: string,
+    category: string,
+    text: string
+}
 
+const ClubCard = (props: cardList) => {
+
+    //<br>태그에 끊어서 span태그 생성
+    const textLines = props.text.split('<br>');
 
     return (
         <div className="main3_card">
-            {/* <div className="card_container"> */}
-                <div className="top">
-                    <div className="card_title">
-                    <h3>프론트엔드 커뮤니티</h3>
-                    </div>
-                    <div className="card_category">
-                        <span>코딩</span>
-                    </div>
+            <div className="top">
+                <div className="card_title">
+                    <h3>{props.title}</h3>
                 </div>
-                <div className="bottom">
-                    <div className="card_detail">
-                        <span>프론트엔드끼리 정보 공유와<br />
-                            소통을 위한 동아리 입니다.
-                        </span>
-                    </div>
+                <div className="card_category">
+                    <span>{props.category}</span>
                 </div>
-            {/* </div> */}
+            </div>
+            <div className="bottom">
+                <div className="card_detail">
+                    {textLines.map((line, index) => (
+                        <span key={index}>{line}</span>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
