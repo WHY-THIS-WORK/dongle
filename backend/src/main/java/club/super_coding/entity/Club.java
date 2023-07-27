56e08720cf9b8a7ea89571e94a1a88401a5bbf9c
김주환 <kjh71047@naver.com>
2023-07-27 23:39:56 +0900

    동아리테이블 -> 카테고리컬럼 분류
    동아리테이블에서 카테고리 항목을 추가 테이블로 정규화 하였습니다.

--- backend/src/main/java/club/super_coding/entity/Club.java
+++ backend/src/main/java/club/super_coding/entity/Club.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
+import org.springframework.stereotype.Controller;
 
 import javax.persistence.*;
 import java.util.Set;
@@ -22,8 +23,8 @@ public class Club {
     @Column(name = "club_id")
     private int clubId;// 동아리ID
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Category category;// 카테고리
+    @Column(name = "category")
+    private int category;// 카테고리
 
     @Column
     private String name;// 동아리명
