71d2bdaeb5b6b2b87e291ccb3fd7de19a4a60ff6
whtnqls124578 <whtnqls124578@naver.com>
2023-07-29 16:35:00 +0900

    늘어지는 css 수정 완료

--- frontend/src/pages/Clubjoin.tsx
+++ frontend/src/pages/Clubjoin.tsx
@@ -55,37 +55,40 @@ const Clubjoin = () => {
         <div className="clubjoin_title">
           <h1>동아리 신청</h1>
         </div>
-        <div className="clubjoin_input name">
-          <JoinInputTitle title="동아리명" />
-          <JoinInputValue placeholder="한/영문 12자 이하"
-          inputHandle={inputHandle}
-            title="name"/>
-        </div>
-        <div className="clubjoin_input categories">
-          <JoinInputTitle title="카테고리" />
-          <JoinBtn inputHandle={inputHandle} title="category" />
-        </div>
-        <div className="clubjoin_input detail">
-          <JoinInputTitle title="동아리 설명" />
-          <JoinInputValue placeholder="동아리에 대한 간단한 설명을 적어주세요."
-          inputHandle={inputHandle}
-            title="description"/>
-        </div>
-        <div className="clubjoin_input direction">
-          <JoinInputTitle title="동아리 활동방향" />
-          <JoinInputTextarea
-            inputHandle={inputHandle}
-            title="direction" />
-        </div>
-        <div className="clubjoin_input notice">
-          <JoinInputTitle title="게시판 선택" />
-          {/* <div className="clubjoin_input-tag">
+        <div className="input_container">
+          <div className="clubjoin_input name">
+            <JoinInputTitle title="동아리명" />
+            <JoinInputValue placeholder="한/영문 12자 이하"
+              inputHandle={inputHandle}
+              title="name" />
+          </div>
+          <div className="clubjoin_input categories">
+            <JoinInputTitle title="카테고리" />
+            <JoinBtn inputHandle={inputHandle} title="category" />
+          </div>
+          <div className="clubjoin_input detail">
+            <JoinInputTitle title="동아리 설명" />
+            <JoinInputValue placeholder="동아리에 대한 간단한 설명을 적어주세요."
+              inputHandle={inputHandle}
+              title="description" />
+          </div>
+          <div className="clubjoin_input direction">
+            <JoinInputTitle title="동아리 활동방향" />
+            <JoinInputTextarea
+              inputHandle={inputHandle}
+              title="direction" />
+          </div>
+          <div className="clubjoin_input notice">
+            <JoinInputTitle title="게시판 선택" />
+            {/* <div className="clubjoin_input-tag">
             <span>JS</span>
           </div> */}
-          <JoinInputValue placeholder="게시판 이름을 입력해주세요."
-          inputHandle={inputHandle}
-            title="boardName"/>
+            <JoinInputValue placeholder="게시판 이름을 입력해주세요."
+              inputHandle={inputHandle}
+              title="boardName" />
+          </div>
         </div>
+        
         <SendBtn text="동아리 신청하기" sendBtnHandle={sendBtnHandle}/>
       </div>
     </div>
